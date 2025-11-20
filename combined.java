7b9d071b1b0b540ca49a13ce9d09034665ac35ae
leimrn <leiladnssmirano@gmail.com>
2025-11-20 18:30:12 +0800

    cm

---
+++ combined.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+  public class combined{
+    public static void main(String []args){
+      Scanner ck = new Scanner(System.in);
+    
+      System.out.print("Enter a number: ");
+      int num = ck.nextInt();
+
+
+    }
+  }
