0e9a19eaf6649bfbe1382cce3e2fa2a9c6d2b995
leimrn <leiladnssmirano@gmail.com>
2025-11-20 20:32:27 +0800

    trials

--- combined.java
+++ combined.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
       System.out.print("Enter a number: ");
       int num = ck.nextInt();
 
+      for(int i = 1; i <= num; i++){
+        for(int j = 1; j <= i; j++){
+          System.out.print("*");
+        }
+        System.out.println("");
+      }
+
+      for(int i = num; i >= 1; i--){
+        for(int j = 1; j <= i; j++){
+          System.out.print("*");
+        }
+        System.out.println("");
+      }
 
     }
   }
